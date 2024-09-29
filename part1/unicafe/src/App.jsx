564364a0258e61be35c01aaf3bb82c7bbc9c8307
amazigh <mazigh@protonmail.com>
2024-09-29 08:20:28 -0400

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,8 +22,11 @@ const App = () => {
       <h1>statistics</h1>
     
       <p>good {good}</p>
-      <p>bad {bad}</p>
       <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad)/(good + neutral + bad)}</p>
+      <p>positive {good/(good + neutral + bad)*100}%</p>
     </div>
   )
 }
