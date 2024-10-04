8378b933d124772d881888902585fae6f5e25919
amazigh <mazigh@protonmail.com>
2024-10-03 22:37:56 -0400

    2.4: Course information step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,67 +1,99 @@
 const Header = ({ course }) => {
   return (
-    <h1>{course.name}</h1>
+    <h2>{course.name}</h2>
   )
 }
 
 const Total = ({ parts }) => {
-  const sum = parts.reduce(
-    (accumulator, currentValue) => {
-      return (accumulator + currentValue.exercises)
-    }, 0
-  )
+    const sum = parts.reduce(
+      (accumulator, currentValue) => {
+        return (accumulator + currentValue.exercises)
+      }, 0
+    )
 
-  return (
-    <p><b>Total of {sum} exercises</b></p>
-  )
-}
+    return (
+      <p><b>Total of {sum} exercises</b></p>
+    )
+  }
 
 const Part = ( { parts } ) => {
     return (
-        parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)
+        parts.map(part => {
+        return <p key={part.id}>{part.name} {part.exercises}</p>
+        }
+      )
     )
-  }
+} 
+  
 
 const Content = ({ parts }) => 
   <>
     <Part parts={parts} />
   </>
     
-const Course = ({course}) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content parts={course.parts} /> 
-    </>
-  )
-} 
-
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+const Course = ({courses}) => {
   return (
     <div>
-      <Course course={course} />
-      <Total parts={course.parts} />
+    {courses.map(course => 
+      <div key={course.id}>
+        <Header course={course} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    )}
+    </div>
+    )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <div id={Math.random()}>
+      <h1>Web development curriculum</h1>
+      <Course courses={courses} />
     </div>
   )
 }
